79528331cc36ffb7147d17d66c01cae27e6a444a
aniket umbc <aniket.umbc@gmail.com>
2018-03-20 19:22:50 +0530

    Add social media icon and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+    './CSS/Footer-with-social-icons.css',
+    './CSS/Socail-icon.css'
+  ]
 })
 export class AppComponent {
   title = 'app';
