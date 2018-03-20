39d124333eeefcedfd41e1b078247b6b7a4dd805
aniket umbc <aniket.umbc@gmail.com>
2018-03-21 00:08:40 +0530

    form added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 fullPathImage:string;
+model:sendData=new sendData();
   constructor() {
     this.fullPathImage='assets/image 2.PNG';
    }
 
   ngOnInit() {
   }
-
 }
+class sendData{
+  constructor(public  name:string='',
+  public  email:string='',
+  public  password:string='',
+  public  comment:string='',){}
+}
+
